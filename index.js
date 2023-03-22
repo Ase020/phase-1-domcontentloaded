8191ee890850ab7a387334f67f6cd751eb8dfc94
felixNyalenda <felixasejunior@gmail.com>
2023-03-22 12:26:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+   const p = document.getElementById("text");
+   p.textContent = "This is really cool!";
+   console.log(p);
+});
+console.log("This is my attempt 2");
